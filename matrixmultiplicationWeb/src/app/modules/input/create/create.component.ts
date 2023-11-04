54a006a57c2ac51bfc28cdfe01092dbbe00bb4e3
cristian <cristian.montenegro@softwareone.com>
2023-11-04 12:57:38 -0500

    mostrar info en crear

--- matrixmultiplicationWeb/src/app/modules/input/create/create.component.ts
+++ matrixmultiplicationWeb/src/app/modules/input/create/create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProcessPpal } from '@app/interfaces/ProcessPpal';
 import { InputCreate } from '@app/models/inputCreate.model';
 import { ApiService } from '@app/services/api-service';
 import { Subject, switchMap } from 'rxjs';
@@ -9,10 +10,12 @@ import { Subject, switchMap } from 'rxjs';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss'],
 })
 
 export class CreateComponent implements OnInit {
   public matrixAForm!: FormGroup;
+  public matrixBForm!: FormGroup;
   process: InputCreate | undefined;
   formSubmitted: boolean = false;
   hasUnsavedChanges: boolean = false;
@@ -34,38 +37,18 @@ export class CreateComponent implements OnInit {
       rows: [null, [Validators.required, Validators.min(1), Validators.max(5), ]],
       columns: [null, [Validators.required, Validators.min(1), Validators.max(5), ]],
     });
+    this.matrixBForm = this.formBuilder.group({
+      rows: [null, [Validators.required, Validators.min(1), Validators.max(5), ]],
+      columns: [null, [Validators.required, Validators.min(1), Validators.max(5), ]],
+    });
   }
 
+  /* */
   get currentProcess(): InputCreate {
     const process = this.matrixAForm.value as InputCreate;
     return process;
   }
-
-  ngOnInit(): void {
-    this.loadProcess();
-  }
   
-  loadProcess(): void {
-    this.activateRoute.params.subscribe((params) => {
-      const processId = +params['id'];
-      if (!isNaN(processId)) {
-        this.loadProcessData(processId);
-      }
-    });
-  }
-  
-  loadProcessData(processId: number): void {
-    this.activateRoute.params
-      .pipe(switchMap(() => this.apiService.getProcessById(processId)))
-      .subscribe((process) => {
-        if (!process) {
-          this.router.navigate(['']);
-        }
-        console.log(process?.matrix[0]);
-        this.matrixAForm.reset(process?.matrix[0]);
-      });
-  }
-
   onSubmit(event: Event): void {
     event.preventDefault();
     this.formSubmitted = true;
@@ -89,8 +72,42 @@ export class CreateComponent implements OnInit {
     }
   }
 
+  /* */
+
+  ngOnInit(): void {
+    this.loadProcess();
+  }
+  
+  loadProcess(): void {
+    this.activateRoute.params.subscribe((params) => {
+      const processId = +params['id'];
+      if (!isNaN(processId)) {
+        this.loadProcessData(processId);
+      }
+    });
+  }
+  
+  loadProcessData(processId: number): void {
+    this.activateRoute.params
+      .pipe(switchMap(() => this.apiService.getProcessById(processId)))
+      .subscribe((process) => {
+        if (!process) {
+          this.router.navigate(['']);
+        }
+        else{
+          this.fillValuesFromDB(process);
+        }
+      });
+  }
+
+  fillValuesFromDB(process: ProcessPpal){
+    this.matrixAForm.reset(process?.matrix[0]);
+    this.matrixBForm.reset(process?.matrix[1]);
+  }
+
   undoChanges() {
     this.matrixAForm.reset();
+    this.matrixBForm.reset();
   }
 
   detectUnsavedChanges() {
